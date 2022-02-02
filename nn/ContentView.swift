c06f059e8c519f8070c9975f2aa28b9a0b17e16d
Reemdeva <alharthira93@gmail.comalharthira93@gmail.com>
2022-02-02 12:04:23 +0300

    Initial Commit

---
+++ nn/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  nn
+//
+//  Created by Reem Alharthi on 01/07/1443 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ nn/nnApp.swift
@@ -0,0 +1,17 @@
+//
+//  nnApp.swift
+//  nn
+//
+//  Created by Reem Alharthi on 01/07/1443 AH.
+//
+
+import SwiftUI
+
+@main
+struct nnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
